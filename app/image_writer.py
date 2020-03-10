9ed748e26d14e358ac51c48478b3690a986f5eb6
John Eastwood <jjge732@gmail.com>
2020-03-09 21:41:37 -0500

    Made class for image methods

--- app/image_writer.py
+++ app/image_writer.py
@@ -2,18 +2,26 @@ import os
 
 ROOT = os.environ.get("FRACTAL_MACHINE_ROOT")
 
-image = open(f"{ROOT}/images/demofile.svg", "w")
-image.write("""
-    <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
-        <rect width="100" height="100" x="000" y="000" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)"/>
-        <rect width="100" height="100" x="000" y="100" style="fill:#00f;stroke-width:3;stroke:rgb(0,0,0)"/>
-        <rect width="100" height="100" x="000" y="200" style="fill:#00f;stroke-width:3;stroke:rgb(0,0,0)"/>
-        <rect width="100" height="100" x="100" y="000" style="fill:#00f;stroke-width:3;stroke:rgb(0,0,0)"/>
-        <rect width="100" height="100" x="100" y="100" style="fill:#00f;stroke-width:3;stroke:rgb(0,0,0)"/>
-        <rect width="100" height="100" x="100" y="200" style="fill:#00f;stroke-width:3;stroke:rgb(0,0,0)"/>
-        <rect width="100" height="100" x="200" y="000" style="fill:#00f;stroke-width:3;stroke:rgb(0,0,0)"/>
-        <rect width="100" height="100" x="200" y="100" style="fill:#00f;stroke-width:3;stroke:rgb(0,0,0)"/>
-        <rect width="100" height="100" x="200" y="200" style="fill:#00f;stroke-width:3;stroke:rgb(0,0,0)"/>
-    </svg>
-""")
-image.close()
+
+class Image:
+    """Class for altering images"""
+
+    @staticmethod
+    def write_image() -> None:
+        """Writes an image in the svg format"""
+
+        image = open(f"{ROOT}/images/demofile.svg", "w")
+        image.write("""
+            <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+                <rect width="100" height="100" x="000" y="000" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)"/>
+                <rect width="100" height="100" x="000" y="100" style="fill:#00f;stroke-width:3;stroke:rgb(0,0,0)"/>
+                <rect width="100" height="100" x="000" y="200" style="fill:#00f;stroke-width:3;stroke:rgb(0,0,0)"/>
+                <rect width="100" height="100" x="100" y="000" style="fill:#00f;stroke-width:3;stroke:rgb(0,0,0)"/>
+                <rect width="100" height="100" x="100" y="100" style="fill:#00f;stroke-width:3;stroke:rgb(0,0,0)"/>
+                <rect width="100" height="100" x="100" y="200" style="fill:#00f;stroke-width:3;stroke:rgb(0,0,0)"/>
+                <rect width="100" height="100" x="200" y="000" style="fill:#00f;stroke-width:3;stroke:rgb(0,0,0)"/>
+                <rect width="100" height="100" x="200" y="100" style="fill:#00f;stroke-width:3;stroke:rgb(0,0,0)"/>
+                <rect width="100" height="100" x="200" y="200" style="fill:#00f;stroke-width:3;stroke:rgb(0,0,0)"/>
+            </svg>
+        """)
+        image.close()
