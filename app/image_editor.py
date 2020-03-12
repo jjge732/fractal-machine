bab0f2ec4e59c6c63aff243e5fd576132d03cf17
John Eastwood <jjge732@gmail.com>
2020-03-11 21:58:56 -0500

    Added encode method to image_writer and made pdf conversion work

--- app/image_editor.py
+++ app/image_editor.py
@@ -11,19 +11,25 @@ class Image:
 
     # TODO: decide way that data should be given to this function from the GUI
     @staticmethod
-    def write_image(color_list: List) -> None:
+    def write_image(color_list: List, file_name: str = "fractal") -> None:
         """Writes an image in the svg format using data from user input in the GUI
 
             Args:
                 color_list: List of lists in the form a 3 x 3 or 4 x 4 square
+                file_name: The name to give the svg file
+
+            Returns:
+                An svg file of the fractal with the given name
+
         """
-        image = None
+        if "." in file_name:
+            file_name = file_name.split('.')[0]
         count = 0
         square_side_length = len(color_list)
-        print(square_side_length)
+        print(f"Creating a square of side length: {square_side_length}")
         while True:
             try:
-                file_name = f"{ROOT}/images/demofile{f'-{count}' if count != 0 else ''}.svg"
+                file_name = f"{ROOT}/images/{f'{file_name}-{count}' if count != 0 else ''}.svg"
                 image = open(file_name, "x")
                 break
             except Exception:
@@ -38,18 +44,22 @@ class Image:
             print(f"Image successfully written as {file_name}")
 
     @staticmethod
-    def convert_to_pdf(colored_square_code: str) -> None:
-        """Method for converting an svg file to pdf
+    def color_code_to_pdf(colored_square_code: str, output_file_name: str = "fractal") -> None:
+        """Method for converting a color_code to a pdf file
 
             Args:
                 colored_square_code: The encoded colored square.
                     Encoded using hex color codes in 9 chunks each of length 3 or 6
                     Obtained from the database.
+                output_file_name:
 
             Returns:
-                File is output to images directory with the same name as the original file.
+                File is output to images directory with the given file name
 
         """
+        if "." in output_file_name:
+            output_file_name = output_file_name.split('.')[0]
+
         colored_square_list = Image.decode_square(colored_square_code)
         pdf = FPDF()
         pdf.add_page()
@@ -57,9 +67,10 @@ class Image:
         for i in colored_square_list:
             for j in range(9):
                 pdf.set_fill_color(r=i["red"], g=i["green"], b=i["blue"])
-                pdf.rect(x=i["index"][0] * 2, y=i["index"][1] * 2, w=2, h=2, style='F')
+                pdf.rect(x=i["index"][0] * 20, y=i["index"][1] * 20, w=20, h=20, style='F')
 
-        pdf.output(f"{ROOT}/images/demo.pdf", 'F')
+        pdf.output(name=f"{ROOT}/images/{output_file_name}.pdf")
+        print(f"File output to {output_file_name}.pdf")
 
     @staticmethod
     def decode_square(colored_square_code: str) -> List:
@@ -76,7 +87,7 @@ class Image:
         """
         if len(colored_square_code) == 27:
             return [{
-                "index": (i % 3, i / 3),
+                "index": (i % 3, int(i / 3)),
                 "red": int(colored_square_code[i * 3] * 2, 16),
                 "green": int(colored_square_code[i * 3 + 1] * 2, 16),
                 "blue": int(colored_square_code[i * 3 + 2] * 2, 16)
@@ -92,20 +103,24 @@ class Image:
             raise Exception("Invalid color encoding.")
 
     @staticmethod
-    def encode_square() -> str:
+    def encode_square(color_list: List) -> str:
         """Method for encoding given square list data to a string
 
             Args:
-                TBD:
+                color_list: List of lists in the form a 3 x 3 or 4 x 4 square
+
+            Returns:
+                An encoded color square using hex color codes in 9 chunks each of length 3 or 6
 
         """
-        pass
+        return "".join("".join(item for item in sub_list) for sub_list in color_list)
 
     @staticmethod
     def convert_to_jpg() -> None:
         """
 
             Returns:
-               .jpf file is saved to images with the same name as the original file
+               .jpg file is saved to images with the same name as the original file
 
         """
+        pass
