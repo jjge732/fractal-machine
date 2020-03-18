b63a6e59efbb040758cf89ca295ab994af95b2e2
Steph99rod <steph@Stephanies-MacBook-Pro.local>
2020-03-18 14:20:00 -0500

    Added four files for theGUI

--- project/app/__main__.py
+++ project/app/__main__.py
@@ -14,6 +14,9 @@ from image_editor import Image
 # ])
 # Image.color_code_to_pdf("000" "000" "FFF" "000" "FFF" "000" "FFF" "000" "000")
 
+
+#get the GUI to generate this stuff
+
 print(Image.encode_square([
     ["000", "000", "FFF"],
     ["000", "FFF", "000"],
